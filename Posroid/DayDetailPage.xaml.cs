209e23fa9e1ca380062b47ccb2721a2122895823
SaschaNaz_cp <SND\SaschaNaz_cp>
2012-07-19 14:46:41 +0000

    디테일 페이지 완성

--- Posroid/DayDetailPage.xaml.cs
+++ Posroid/DayDetailPage.xaml.cs
@@ -37,6 +37,8 @@ namespace Posroid
         /// session.  This will be null the first time a page is visited.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
+            if (navigationParameter != null)
+                this.DefaultViewModel["MealTimes"] = (navigationParameter as Day).Times;
         }
 
         /// <summary>
